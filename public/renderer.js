faefab3194b1eb3c1117307e9d9e5ec8c55ccf20
Ronny Mathew <ronnygeo@Ronnys-MacBook-Pro.local>
2017-02-25 15:17:48 -0500

    Working boilerplate electron.

---
+++ public/renderer.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ronnygeo on 2/25/17.
+ */
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
