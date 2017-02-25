0cc72ee153da63f72558a8097bdc3992c628051b
Sumeet Dubey <sumeetddubey@gmail.com>
2017-02-25 15:13:15 -0500

    added some mock data

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 
-mongoose.connect(connectionString);
+//mongoose.connect(connectionString);
 
 //server javascript
 require("./app.js")(app, mongoose);
